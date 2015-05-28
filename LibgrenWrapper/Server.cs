a16df004204275698117174ce8c2eb21d4eaa8f6
Wouter Kosse <kosse.w@live.nl>
2015-05-28 10:24:44 +0200

    Hoi

--- LibgrenWrapper/Server.cs
+++ LibgrenWrapper/Server.cs
@@ -28,9 +28,56 @@ namespace LibgrenWrapper
 
         private void TimerOnTick(object sender, EventArgs eventArgs)
         {
-            
-        }
+                NetIncomingMessage im;
+                while ((im = s_server.ReadMessage()) != null)
+                {
+                    // handle incoming message
+                    switch (im.MessageType)
+                    {
+                        case NetIncomingMessageType.DebugMessage:
+                        case NetIncomingMessageType.ErrorMessage:
+                        case NetIncomingMessageType.WarningMessage:
+                        case NetIncomingMessageType.VerboseDebugMessage:
+                            string text = im.ReadString();
+                            Output(text);
+                            break;
 
+                        case NetIncomingMessageType.StatusChanged:
+                            NetConnectionStatus status = (NetConnectionStatus)im.ReadByte();
+
+                            string reason = im.ReadString();
+                            Output(NetUtility.ToHexString(im.SenderConnection.RemoteUniqueIdentifier) + " " + status + ": " + reason);
+
+                            if (status == NetConnectionStatus.Connected)
+                                Output("Remote hail: " + im.SenderConnection.RemoteHailMessage.ReadString());
+
+                            UpdateConnectionsList();
+                            break;
+                        case NetIncomingMessageType.Data:
+                            // incoming chat message from a client
+                            string chat = im.ReadString();
+
+                            Output("Broadcasting '" + chat + "'");
+
+                            // broadcast this to all connections, except sender
+                            List<NetConnection> all = s_server.Connections; // get copy
+                            all.Remove(im.SenderConnection);
+
+                            if (all.Count > 0)
+                            {
+                                NetOutgoingMessage om = s_server.CreateMessage();
+                                om.Write(NetUtility.ToHexString(im.SenderConnection.RemoteUniqueIdentifier) + " said: " + chat);
+                                s_server.SendMessage(om, all, NetDeliveryMethod.ReliableOrdered, 0);
+                            }
+                            break;
+                        default:
+                            Output("Unhandled type: " + im.MessageType + " " + im.LengthBytes + " bytes " + im.DeliveryMethod + "|" + im.SequenceChannel);
+                            break;
+                    }
+                    s_server.Recycle(im);
+                }
+        }
+        
         private static void Output(string text)
         {
             Console.WriteLine(text);
