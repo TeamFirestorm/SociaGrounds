d4b02011f4ba292eadfe71d4104810aa65682ad8
Wouter Kosse <kosse.w@live.nl>
2015-05-28 13:06:58 +0200

    DatabaseStuff

--- LibgrenWrapper/DataBase.cs
+++ LibgrenWrapper/DataBase.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net;
 
 namespace LibgrenWrapper
 {
-    class DataBase
+    public class DataBase
     {
+
+        public bool insertConnectionInfo(string ipaddress, string dnssuffix)
+        {
+            var request = (HttpWebRequest)WebRequest.Create("http://www.matthijsreeringh.nl/SociaGrounds/insertConnectionInfo.php?ipadress="+ipaddress+"&dnssuffix="+dnssuffix);
+            request.GetResponse();
+            
+        }
+
     }
 }
