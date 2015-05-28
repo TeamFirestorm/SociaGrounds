de6179e5b1531b0bd188be5b75c0a346798b993c
Thijs Reeringh <mreeringh@hotmail.com>
2015-05-28 11:29:02 +0200

    Added Client

--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using LibgrenWrapper;
 
 namespace Client
 {
@@ -23,6 +24,18 @@ namespace Client
         public MainWindow()
         {
             InitializeComponent();
+            LibgrenWrapper.Client.Setup();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            LibgrenWrapper.Client.Send(txtMessage.Text);
+            txtMessage.Text = "";
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            LibgrenWrapper.Client.Connect("10.110.110.191", 14242);            
         }
     }
 }
