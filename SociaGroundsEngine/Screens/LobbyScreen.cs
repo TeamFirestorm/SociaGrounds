191d26c2cfb5ffff87a5e08e87ea959305c5a59d
Thijs Reeringh <mreeringh@hotmail.com>
2015-05-31 13:31:30 +0200

    Added Lobbycreen

---
+++ SociaGroundsEngine/Screens/LobbyScreen.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SociaGroundsEngine.Screens
+{
+    public class LobbyScreen : Screen
+    {
+        private bool sendRequest;
+        private string result;
+
+        public override void update()
+        {
+            
+        }
+
+        public override void draw(Microsoft.Xna.Framework.Graphics.SpriteBatch spriteBatch)
+        {
+            
+        }
+    }
+}
