a43863eab0edc96075b0b0437a2e3840903dbf29
Gyllion <gyllion.van.elderen@student.stenden.com>
2015-06-15 14:50:00 +0200

    Camera fix

--- SocialGroundsStore/Camera.cs
+++ SocialGroundsStore/Camera.cs
@@ -22,36 +22,36 @@ namespace SocialGroundsStore
         public void Update(Viewport viewport, Vector2 position, Map map, Ui ui)
         {
             //// If the camera is on the left side of the map, stop the horizontal camera movement
-            if (position.X < map.StartPosition.X + (viewport.Width / 2))
+            if (position.X < map.StartPosition.X + (viewport.Width / 4f))
             {
-                centre.X = map.StartPosition.X + (viewport.Width / 2);
+                centre.X = map.StartPosition.X + (viewport.Width / 4f);
             }
-            //// If the camera is on the right side of the map , stop the horizontal camera movement
-            //else if (position.X > map.StartPosition.X + (map.MapWidth - (viewport.Width / 2)))
-            //{
-            //    centre.X = map.StartPosition.X + (map.MapWidth - (viewport.Width / 2));
-            //}
-            //// Horizontal camera movement
-            else //if (position.X >  map.StartPosition.X + (viewport.Width / 2) && position.X < map.StartPosition.X + (map.MapWidth - (viewport.Width / 3)))
+            // If the camera is on the right side of the map , stop the horizontal camera movement
+            else if (position.X > map.StartPosition.X + (map.MapWidth - (viewport.Width / 3f)))
+            {
+                centre.X = map.StartPosition.X + (map.MapWidth - (viewport.Width / 3f));
+            }
+            // Horizontal camera movement
+            else
             {
                 centre.X = position.X;
             }
 
             //// If the camera is on the top of the map, stop the vertical camera movement
-            //if (position.Y < map.StartPosition.Y + (viewport.Height / 2))
-            //{
-            //    centre.Y = map.StartPosition.Y + (viewport.Height / 2);
-            //}
-            //// If the camera is on the bottom of the map, stop the vertical camera movement
-            //else if (position.Y > (map.StartPosition.Y + map.MapHeight) - (viewport.Height / 2))
-            //{
-            //    centre.Y = (map.StartPosition.Y + map.MapHeight) - (viewport.Height / 2);
-            //}
+            if (position.Y < map.StartPosition.Y + (viewport.Height / 4f))
+            {
+                centre.Y = map.StartPosition.Y + (viewport.Height / 4f);
+            }
+            // If the camera is on the bottom of the map, stop the vertical camera movement
+            else if (position.Y > (map.StartPosition.Y + map.MapHeight) - (viewport.Height / 3f))
+            {
+                centre.Y = (map.StartPosition.Y + map.MapHeight) - (viewport.Height / 3f);
+            }
             // Vertical camera movement
-            //else if (position.Y < map.StartPosition.Y + (viewport.Height / 2) && position.Y > (map.StartPosition.Y + map.MapHeight) - (viewport.Height / 2))
-            //{
+            else
+            {
                 centre.Y = position.Y;
-            //}
+            }
 
             // Camera translation
             transformPrivate = Matrix.CreateTranslation(new Vector3(-centre.X + (viewport.Height / 2.5f), -centre.Y + (viewport.Width / 8), 0)) * Matrix.CreateScale(2.0f, 2.0f, 0);
