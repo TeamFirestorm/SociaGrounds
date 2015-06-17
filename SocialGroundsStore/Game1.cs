5bf1c3169345df0131a1fb95fc0a54e8fc4f5cd8
Thijs Reeringh <mreeringh@hotmail.com>
2015-06-17 13:13:12 +0200

    Changed

--- SocialGroundsStore/Game1.cs
+++ SocialGroundsStore/Game1.cs
@@ -18,7 +18,7 @@ namespace SocialGroundsStore
         SpriteBatch _spriteBatch;
         public static Texture2D texture;
         public static SpriteFont font;
-        public static int sendTime = 10;
+        public static int sendTime = 1;
 
 
         public enum ScreenState
