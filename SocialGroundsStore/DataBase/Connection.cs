c59e421bb70e5821e2d155086292e5a16464412b
Thijs Reeringh <mreeringh@hotmail.com>
2015-06-19 10:05:37 +0200

    Changed Conenction

--- SocialGroundsStore/DataBase/Connection.cs
+++ SocialGroundsStore/DataBase/Connection.cs
@@ -1,6 +1,9 @@
 ﻿namespace SocialGroundsStore.DataBase
 {
     // ReSharper disable once ClassNeverInstantiated.Global
+    /// <summary>
+    /// 
+    /// </summary>
     public class Connection
     {
         public string IpAddress { get; set; }
