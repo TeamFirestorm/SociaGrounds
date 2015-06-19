8a223d1843521472e06ae384d43912ec6de6de65
Thijs Reeringh <mreeringh@hotmail.com>
2015-06-19 09:18:05 +0200

    Added chnaged UI

---
+++ SocialGroundsStore/GUI/GUI.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace SocialGroundsStore.GUI
+{
+    public class Gui
+    {
+        private readonly RealKeyBoard _keyboard;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="content"></param>
+        public Gui(ContentManager content)
+        {
+            _keyboard = new RealKeyBoard(content);
+        }
+
+        /// <summary>
+        /// Update
+        /// </summary>
+        /// <param name="position"></param>
+        /// <param name="viewport"></param>
+        /// <param name="keyState"></param>
+        public void Update(Vector2 position, Viewport viewport, KeyboardState keyState)
+        {
+            _keyboard.Update(position,viewport,keyState);
+        }
+
+        /// <summary>
+        /// Draw
+        /// </summary>
+        /// <param name="spriteBatch"></param>
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            _keyboard.Draw(spriteBatch);
+        }
+    }
+}
